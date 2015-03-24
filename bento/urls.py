1cf34db88e8533f0cdc0a47e04cfd4bcf9c470c0
a-ttitude <a-ttitude@outlook.com>
2015-03-24 16:03:23 +0100

    Ajout des pages connexion et inscription.

--- bento/urls.py
+++ bento/urls.py
@@ -2,8 +2,10 @@
 
 from django.conf.urls import patterns, url
 
-from bento.views import index
+from bento.views import index, login, signup
 
 urlpatterns = patterns('',
                        url(r'^$', index, name='index'),
+                       url(r'^login$', login, name='login'),
+                       url(r'^signup$', signup, name='signup'),
                        )
