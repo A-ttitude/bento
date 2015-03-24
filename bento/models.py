c537179bee923258c94058d41396c78a0751a090
Username69 <quentoche@hotmail.com>
2015-03-24 15:28:33 +0100

    Admin & Models

--- bento/models.py
+++ bento/models.py
@@ -1,5 +1,30 @@
-#coding: utf-8
+# coding: utf-8
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+# class User(models.Model):
+# nickname = models.CharField(max_length=50)
+#     password = models.CharField(max_length=50)
+#     dateRegistration = models.DateField(auto_now=True)
+#
+#     def __str__(self):
+#         return self.nickname
+#
+#     class Meta:
+#         verbose_name = _("Utilisateur")
+#         verbose_name_plural = _("Utilisateurs")
+
+
+# class Recipe(models.Model):
+#     owner = models.ForeignKey('User')
+#     name = models.CharField(max_length=255)
+#     note = models.PositiveIntegerField(default=0)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         verbose_name = _("Recette")
+S#         verbose_name_plural = _("Recettes")
