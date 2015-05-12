4c970508b69febf84d27eae6b43712fe01ab0872
a-ttitude <a-ttitude@outlook.com>
2015-05-12 22:02:29 +0200

    Hélène : Adaptation des 2 projets

--- bento/admin.py
+++ bento/admin.py
@@ -1,7 +1,15 @@
 #coding: utf-8
 
 from django.contrib import admin
+from bento.models import Ingredients, DifficulteRecette, TypeRecette, Recette, Commentaires
 
-class UserAdmin(admin.ModelAdmin):
-    list_filter = ('nickname',)
-    date_hierarchy = 'date'
+
+admin.site.register(Recette)
+admin.site.register(TypeRecette)
+admin.site.register(Commentaires)
+admin.site.register(Ingredients)
+
+
+#class UserAdmin(admin.ModelAdmin):
+#   list_filter = ('nickname',)
+#    date_hierarchy = 'date'
--- bento/urls.py
+++ bento/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, url
 from bento.views import index, login, signup
 
 urlpatterns = patterns('',
-                       url(r'^$', index, name='index'),
-                       url(r'^login$', login, name='login'),
-                       url(r'^signup$', signup, name='signup'),
+                       url(r'^$', index, name='accueil'),
+                       url(r'^login$', login, name='connexion'),
+                       url(r'^signup$', signup, name='enregistrer'),
                        )
